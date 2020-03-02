67a13e8b2e39760b7b59b34b4e3f8997edd94b66
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-03-02 19:06:42 +0300

    finished about page

--- src/styles.js
+++ src/styles.js
@@ -6,6 +6,7 @@ export const StyledApp = styled.div`
   background-color: #42240c;
   font-family: "Monofett";
   color: #42e6a4;
+  overflow-y:auto;
 `;
 
 export const StyledBodyCont = styled.div`
@@ -31,20 +32,21 @@ export const StyledBodyCont = styled.div`
       width: 50%;
       margin-top: 5%;
       color: #42240c;
-      border-radius:2%;
-      h2 { 
+      border-radius: 2%;
+      h2 {
         font-weight: normal;
-        padding-left:3%;
+        padding-left: 3%;
       }
       p {
-        padding:3%;
-        padding-top:0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding: 3%;
+        padding-top: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+          Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
       }
       li {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+          Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
       }
-
     }
     .authentication {
       background-color: #42e6a4;
@@ -52,11 +54,65 @@ export const StyledBodyCont = styled.div`
       width: 20%;
       margin-top: 5%;
       color: #42240c;
-      display:flex;
-      flex-direction:column;
-      border-radius:2%;
+      display: flex;
+      flex-direction: column;
+      border-radius: 2%;
+    }
+  }
+`;
 
-
+export const StyledAboutBodyCont = styled.div`
+  width: 95%;
+  height: 95%;
+  background-color: #42240c;
+  color: #42e6a4;
+  h1 {
+    margin: 0;
+    font-weight: normal;
+    padding: 2%;
+    margin-bottom:5%;
+  }
+  h2 {
+    margin: 0;
+    font-weight: normal;
+    padding: 2%;
+    font-size:1.5em;
+  }
+  .Team {
+    width: 95%;
+    height: 95%;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    margin:0;
+    .team-member {
+      width: 38%;
+      height: 45%;
+      display: flex;
+      flex-direction: column;
+      background-color: #42e6a4;
+      color: #42240c;
+      align-items: center;
+      margin-left: 4%;
+      img {
+        padding-top: 5%;
+        width: 60%;
+        height: 60%;
+        border-radius: 50%;
+      }
+      h4 {
+        font-weight: normal;
+        font-size: 1.2em;
+        margin: 0;
+        padding: 0;
+      }
+      a {
+        margin-left: 35%;
+        .github-logo {
+          width: 48%;
+          height: 45%;
+        }
+      }
     }
   }
 `;
